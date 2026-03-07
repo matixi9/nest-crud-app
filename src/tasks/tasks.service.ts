b1306f3abde4d462ab58da8fe3e7f9994fba28a6
matixi9 <290216@student.pwr.edu.pl>
2026-03-07 18:04:58 +0100

    Zainicjowanie repo

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
+
+@Injectable()
+export class TasksService {
+
+  private tasks: Task[] = [];
+  private idCounter: number = 1;
+
+  create(createTaskDto: CreateTaskDto) {
+    return 'This action adds a new task';
+  }
+
+  findAll() {
+    return `This action returns all tasks`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} task`;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return `This action updates a #${id} task`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} task`;
+  }
+}
